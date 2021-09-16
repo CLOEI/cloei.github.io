d35c9c307ed967028fd467e3f61f7e25a1c41c9e
Cendy <cendyemail@gmail.com>
2021-09-16 12:37:37 +0700

    Fixed Config

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/ac4da26e-ae13-472f-b94e-b454a1dd4e00/deploy-status)](https://app.netlify.com/sites/happy-hamilton-bbb6af/deploys)
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Cendy",
-  },
-  plugins: [
-    "gatsby-plugin-netlify-cms",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+	siteMetadata: {
+		siteUrl: "https://www.yourdomain.tld",
+		title: "Cendy",
+	},
+	plugins: [
+		"gatsby-plugin-netlify-cms",
+		"gatsby-plugin-image",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: `${__dirname}/static/img/`,
+			},
+			__key: "images",
+		},
+	],
 };
